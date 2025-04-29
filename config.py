1ec63bdbc2c12706b71e3cfebb179509a714a103
appxextractorbot <appxextractorbot@gmail.com>
2025-04-29 11:11:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
+api_id = "27058143"
+api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
 bot_token = "enter your bot token"
-auth_users = [7991495621]
+auth_users = [8144269730]
